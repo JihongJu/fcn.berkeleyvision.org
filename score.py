874ea0f3df2dfd063e6c97c9407714e8b57093c0
Jihong Ju <daniel.jihong.ju@gmail.com>
2016-11-16 22:24:12 +0100

    Add seg_trains to validate the scores for the train set

--- score.py
+++ score.py
@@ -34,6 +34,10 @@ def seg_tests(solver, save_format, dataset, layer='score', gt='label'):
     solver.test_nets[0].share_with(solver.net)
     do_seg_tests(solver.test_nets[0], solver.iter, save_format, dataset, layer, gt)
 
+def seg_trains(solver, save_format, dataset, layer='score', gt='label'):
+    print '>>>', datetime.now(), 'Begin seg tests'
+    do_seg_tests(solver.net, solver.iter, save_format, dataset, layer, gt)
+
 def do_seg_tests(net, iter, save_format, dataset, layer='score', gt='label'):
     n_cl = net.blobs[layer].channels
     if save_format:
@@ -54,3 +58,4 @@ def do_seg_tests(net, iter, save_format, dataset, layer='score', gt='label'):
     print '>>>', datetime.now(), 'Iteration', iter, 'fwavacc', \
             (freq[freq > 0] * iu[freq > 0]).sum()
     return hist
+
